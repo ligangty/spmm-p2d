67714a0e0abeaed04890757a7a8db385b5b4b087
Gang Li <ligangty@users.noreply.github.com>
2022-05-19 17:47:00 +0800

    Some adjustment for fake-oc

--- fake-oc/go.mod
+++ fake-oc/go.mod
@@ -3,12 +3,7 @@ module github.com/ligangty/fake-oc
 go 1.16
 
 require (
-	github.com/manifoldco/promptui v0.9.0
-	github.com/openshift/api v3.9.0+incompatible // indirect
-	github.com/openshift/client-go v3.9.0+incompatible // indirect
 	github.com/spf13/cobra v1.4.0
 	k8s.io/apimachinery v0.23.5
-	k8s.io/cli-runtime v0.23.5 // indirect
 	k8s.io/client-go v0.23.5
-	k8s.io/klog v1.0.0 // indirect
 )
--- fake-oc/sts.go
+++ fake-oc/sts.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/spf13/cobra"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
 
@@ -30,7 +32,18 @@ func listSTSCmd() *cobra.Command {
 }
 
 func doListSTS(kubeconfig, project string) {
-	clientset := GetKubeClient(kubeconfig)
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
 	namespace := project
 	dps, err := clientset.AppsV1().StatefulSets(namespace).List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
